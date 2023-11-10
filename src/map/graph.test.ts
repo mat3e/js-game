2de70f0bdff1b1c3762c33739de429cebd689283
Mateusz Chrzonstowski <mateusz.chrzonstowski@paramount.com>
2023-11-10 23:51:52 +0100

    Add Graph

---
+++ src/map/graph.test.ts
@@ -0,0 +1,103 @@
+import {DiagonalGraph, Graph, unsafeCost} from "./graph";
+
+const {objectContaining} = expect;
+
+describe('Graph', () => {
+    it('treats positive number as a valid neighbor and skips neighbors for 0', () => {
+        const graph = new Graph([
+            [1, 0],
+            [1, 2],
+        ]);
+
+        // when
+        const zeroZeroNeighbors = graph.neighborsOf({x: 0, y: 0});
+        const oneZeroNeighbors = graph.neighborsOf({x: 1, y: 0});
+        const zeroOneNeighbors = graph.neighborsOf({x: 0, y: 1});
+        const oneOneNeighbors = graph.neighborsOf({x: 1, y: 1});
+
+        // then
+        expect(zeroZeroNeighbors.size).toBe(1);
+        expect(zeroZeroNeighbors).toContainEqual(objectContaining({x: 0, y: 1}));
+        // and
+        expect(oneZeroNeighbors.size).toBe(0);
+        // and
+        expect(zeroOneNeighbors.size).toBe(2)
+        expect(zeroOneNeighbors).toContainEqual(objectContaining({x: 0, y: 0}));
+        expect(zeroOneNeighbors).toContainEqual(objectContaining({x: 1, y: 1}));
+        // and
+        expect(oneOneNeighbors.size).toBe(1);
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 0, y: 1}));
+    });
+
+    it('is just Graph not DiagonalGraph', () => {
+        expect(new Graph([])).toBeInstanceOf(Graph);
+        expect(new Graph([])).not.toBeInstanceOf(DiagonalGraph);
+    });
+});
+
+describe('DiagonalGraph', () => {
+    it('treats positive number as a valid neighbor and skips neighbors for 0', () => {
+        const graph = new DiagonalGraph([
+            [1, 3, 0],
+            [1, 2, 9],
+            [0, 0, 4],
+        ]);
+
+        // when
+        const oneOneNeighbors = graph.neighborsOf({x: 1, y: 1});
+
+        // then
+        expect(oneOneNeighbors.size).toBe(5);
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 0, y: 0}));
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 0, y: 1}));
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 0, y: 1}));
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 2, y: 1}));
+        expect(oneOneNeighbors).toContainEqual(objectContaining({x: 2, y: 2}));
+    });
+
+    it('is Graph and DiagonalGraph', () => {
+        expect(new DiagonalGraph([])).toBeInstanceOf(Graph);
+        expect(new DiagonalGraph([])).toBeInstanceOf(DiagonalGraph);
+    });
+});
+
+describe('unsafeCost', () => {
+    it('throws when not GraphPoint', () => {
+        expect(() => unsafeCost({x: 0, y: 0}, {x: 1, y: 1})).toThrow();
+    });
+
+    it('returns weight ignoring the distance', () => {
+        // given
+        const simpleGraph = new Graph([[1, 2, 3, 4, 5]]);
+        const weight2 = [...simpleGraph.neighborsOf({x: 0, y: 0})][0]!;
+        const weight4 = [...simpleGraph.neighborsOf({x: 4, y: 0})][0]!;
+
+        expect(unsafeCost({x: 0, y: 0}, weight2)).toBe(2);
+        expect(unsafeCost({x: 0, y: 0}, weight4)).toBe(4);
+        // and
+        expect(unsafeCost(weight2, weight4)).toBe(4);
+        expect(unsafeCost(weight4, weight2)).toBe(2);
+    });
+
+    it('returns weight ignoring the distance for diagonal, using square when needed', () => {
+        // given
+        const simpleGraph = new DiagonalGraph([
+            [9, 1, 0, 0, 0],
+            [0, 0, 2, 0, 0],
+            [0, 4, 0, 3, 5],
+        ]);
+        const weight1 = [...simpleGraph.neighborsOf({x: 0, y: 0})][0]!;
+        const weight2 = [...simpleGraph.neighborsOf({x: 1, y: 2})][0]!;
+        const weight3 = [...simpleGraph.neighborsOf({x: 4, y: 2})][0]!;
+
+        expect(unsafeCost({x: 0, y: 0}, weight1)).toBe(1);
+        expect(unsafeCost({x: 1, y: 1}, weight2)).toBe(2);
+        expect(unsafeCost({x: 0, y: 2}, weight3)).toBe(3);
+        // and diagonal
+        expect(unsafeCost(weight1, weight2)).toBe(2 * Math.SQRT2);
+        expect(unsafeCost(weight2, weight1)).toBe(Math.SQRT2);
+        expect(unsafeCost(weight2, weight3)).toBe(3 * Math.SQRT2);
+        expect(unsafeCost(weight3, weight2)).toBe(2 * Math.SQRT2);
+        expect(unsafeCost(weight3, weight1)).toBe(Math.SQRT2);
+    });
+});
---
+++ src/map/graph.ts
@@ -0,0 +1,111 @@
+export type Point = {
+    x: number;
+    y: number;
+}
+
+export class Graph {
+    #adjacency: Map<string, Set<string>> = new Map();
+    #originalPoints: Map<string, GraphPoint> = new Map();
+
+    constructor(grid: number[][]) {
+        grid.forEach((row, y) => row.forEach((value, x) => {
+            if (value > 0) {
+                const point = this.createPoint(x, y, value);
+                const pointStr = point.toString();
+                this.#originalPoints.set(pointStr, point);
+                this.#adjacency.set(pointStr, new Set());
+                this.#addConnections(point);
+            }
+        }));
+    }
+
+    neighborsOf(point: Point): Set<Point> {
+        return new Set([...this.#adjacency.get(toString(point)) ?? []]
+            .map(neighbor => this.#originalPoints.get(neighbor)!));
+    }
+
+    protected createPoint(x: number, y: number, weight: number): GraphPoint {
+        return new FourNeighborsPoint(x, y, weight);
+    }
+
+    #addConnections(point: GraphPoint) {
+        point.potentialNeighbors.forEach(neighbor => {
+            const neighborStr = neighbor.toString();
+            const pointStr = point.toString();
+            if (this.#adjacency.has(neighborStr)) {
+                this.#adjacency.get(pointStr)!.add(neighborStr);
+                this.#adjacency.get(neighborStr)!.add(pointStr);
+            }
+        });
+    }
+}
+
+export class DiagonalGraph extends Graph {
+    protected override createPoint(x: number, y: number, weight: number): GraphPoint {
+        return new DiagonalNeighborsPoint(x, y, weight);
+    }
+}
+
+/**
+ * Should be more restrict, validate and use better cost calculation, but we have a full control over calling it
+ * and optimize toward performance.
+ *
+ * Should be fine when calling it just for the current node and its corresponding neighbors.
+ */
+export function unsafeCost(from: Point, to: Point): number {
+    return (to as GraphPoint).costFrom(from);
+}
+
+abstract class GraphPoint implements Point {
+    constructor(readonly x: number, readonly y: number, protected readonly weight: number = 1) {
+    }
+
+    abstract get potentialNeighbors(): Set<Point>;
+
+    // should multiply by x and y differences
+    costFrom({}: Point): number {
+        return this.weight;
+    }
+
+    toString(): string {
+        return toString(this);
+    }
+}
+
+function toString({x, y}: Point): string {
+    return `${x}x${y}`;
+}
+
+class FourNeighborsPoint extends GraphPoint {
+    override get potentialNeighbors(): Set<Point> {
+        return new Set([
+            new FourNeighborsPoint(this.x - 1, this.y),
+            new FourNeighborsPoint(this.x + 1, this.y),
+            new FourNeighborsPoint(this.x, this.y - 1),
+            new FourNeighborsPoint(this.x, this.y + 1)
+        ]);
+    }
+}
+
+class DiagonalNeighborsPoint extends GraphPoint {
+    override get potentialNeighbors(): Set<Point> {
+        return new Set([
+            new DiagonalNeighborsPoint(this.x - 1, this.y - 1),
+            new DiagonalNeighborsPoint(this.x, this.y - 1),
+            new DiagonalNeighborsPoint(this.x + 1, this.y - 1),
+            new DiagonalNeighborsPoint(this.x - 1, this.y),
+            new DiagonalNeighborsPoint(this.x + 1, this.y),
+            new DiagonalNeighborsPoint(this.x - 1, this.y + 1),
+            new DiagonalNeighborsPoint(this.x, this.y + 1),
+            new DiagonalNeighborsPoint(this.x + 1, this.y + 1),
+        ]);
+    }
+
+    // should multiply by x and y differences
+    override costFrom({x, y}: Point): number {
+        if (x !== this.x && y !== this.y) {
+            return this.weight * Math.SQRT2;
+        }
+        return this.weight;
+    }
+}
